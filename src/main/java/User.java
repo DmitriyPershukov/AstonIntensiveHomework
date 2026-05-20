7f7d7a73436e167cafa56da5b3b4699d51bc2318
DmitriyPershukov <dmitriypershukov@gmail.com>
2026-05-20 18:53:38 +0500

    added User entity

---
+++ src/main/java/User.java
@@ -0,0 +1,56 @@
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter @NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String name;
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Column(nullable = false)
+    @Min(value = 18)
+    private int age;
+    @Column(name = "created_at",nullable = false)
+    @Setter(AccessLevel.NONE)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    private void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public User(String name, String email, int age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    public void setAge(int age) {
+        if (age < 0){
+            throw new IllegalArgumentException("Возраст пользователя не может быть отрицательным");
+        }
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                ", createdAt=" + createdAt +
+                '}';
+    }
+}
