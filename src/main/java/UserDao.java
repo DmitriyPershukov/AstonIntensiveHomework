bec998f1615adfb274f54854e34a0165a311e325
DmitriyPershukov <dmitriypershukov@gmail.com>
2026-05-20 18:53:58 +0500

    added User dao interface

---
+++ src/main/java/UserDao.java
@@ -0,0 +1,16 @@
+import java.util.List;
+import java.util.Optional;
+
+public interface UserDao {
+    Optional<User> findById(Long id);
+
+    List<User> findAll();
+
+    void save(User user);
+
+    void updateById(Long id, User user);
+
+    void deleteById(Long id);
+
+    boolean existsById(Long id);
+}
