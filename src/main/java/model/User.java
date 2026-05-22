e85e47fd3f86a95657495d8a5d3e35836a540622
DmitriyPershukov <dmitriypershukov@gmail.com>
2026-05-22 19:45:46 +0500

    removed setters and refactored annotations to reflect constraitns

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,10 +1,7 @@
 package model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,19 +13,26 @@ import java.time.LocalDateTime;
 @Getter @Setter @NoArgsConstructor
 @Table(name = "users")
 public class User {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Setter(AccessLevel.NONE)
     private Long id;
+
     @Column(unique = true, nullable = false)
     @Size(min = 4, max = 20)
+    @NotBlank
     private String name;
+
     @Column(unique = true, nullable = false)
-    @Size(min = 4, max = 20)
+    @Size(min = 4, max = 100)
+    @NotBlank
     @Email
     private String email;
+
     @Column(nullable = false)
     @Min(value = 18)
     private int age;
+
     @Column(name = "created_at",nullable = false)
     @Setter(AccessLevel.NONE)
     private LocalDateTime createdAt;
@@ -39,35 +43,8 @@ public class User {
     }
 
     public User(String name, String email, int age) {
-        setName(name);
-        setEmail(email);
-        setAge(age);
-    }
-
-    public void setName(String name) {
-        if (name.length() < 4){
-            throw new IllegalArgumentException("Имя пользователя не должно быть короче 4 символов.");
-        }
-        if (name.length() > 20){
-            throw new IllegalArgumentException("Имя пользователя не должно быть длиннее 20 символов.");
-        }
         this.name = name;
-    }
-
-    public void setEmail(String email) {
-        if (name.length() < 4){
-            throw new IllegalArgumentException("Пароль не должнен быть короче 4 символов.");
-        }
-        if (name.length() > 20){
-            throw new IllegalArgumentException("Пароль не должнен быть длиннее 20 символов.");
-        }
         this.email = email;
-    }
-
-    public void setAge(int age) {
-        if (age < 18){
-            throw new IllegalArgumentException("Возраст пользователя не может быть меньше 18.");
-        }
         this.age = age;
     }
 
