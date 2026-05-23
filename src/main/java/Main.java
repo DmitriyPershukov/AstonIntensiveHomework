a66dd1a84ed548a73b74572caf3ad078f973cd28
DmitriyPershukov <dmitriypershukov@gmail.com>
2026-05-23 19:15:22 +0500

    Main.main now uses new interface as var type

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,11 @@
+import model.User;
+import persistence.DataAccessObject;
 import persistence.HibernateUserDao;
-import persistence.UserDao;
 import ui.UserInterface;
 
 public class Main {
     public static void main(String[] args){
-        UserDao userDao = new HibernateUserDao();
+        DataAccessObject<User> userDao = new HibernateUserDao();
         UserInterface userInterface = new UserInterface();
         userInterface.interactWithUser(userDao);
     }
