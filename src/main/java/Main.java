3707fc5c6dc68fa44d4e1343c80ea6a07c5a8856
DmitriyPershukov <dmitriypershukov@gmail.com>
2026-05-23 21:01:36 +0500

    Made Main.main method use generic dao

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,12 @@
 import model.User;
 import persistence.DataAccessObject;
+import persistence.HibernateDao;
 import persistence.HibernateUserDao;
 import ui.UserInterface;
 
 public class Main {
     public static void main(String[] args){
-        DataAccessObject<User> userDao = new HibernateUserDao();
+        DataAccessObject<User> userDao = new HibernateDao(User.class);
         UserInterface userInterface = new UserInterface();
         userInterface.interactWithUser(userDao);
     }
