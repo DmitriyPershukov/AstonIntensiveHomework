af811daaa8e4c05549d1c7af1806b891423467ac
DmitriyPershukov <dmitriypershukov@gmail.com>
2026-05-27 19:58:49 +0500

    fixed bugs

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -18,7 +18,7 @@ public class UserController {
         this.userService = userService;
     }
 
-    public void interactWithUser(DataAccessObject<User> userDao){
+    public void interactWithUser(){
         logger.info("Starting user console interaction.");
         while(true){
             System.out.println("Введите код операции:");
@@ -93,9 +93,10 @@ public class UserController {
         try{
             userService.createUser(newUser);
             System.out.println("Пользователь успешно сохранен");
-        } catch (ConstraintViolationException ex){
-            String.format("При попытке добавить пользователя произошла ошибка: %s",
-                    ex.getMessage());
+        } catch (org.hibernate.exception.ConstraintViolationException
+                 | jakarta.validation.ConstraintViolationException ex){
+            System.out.println(String.format("При попытке добавить пользователя произошла ошибка: %s",
+                    ex.getMessage()));
         }
     }
 
@@ -118,10 +119,11 @@ public class UserController {
         }
         try {
             userService.updateUser(id.get(), newUser);
-            String.format("Пользователь с id %d успешно обновлен.", id.get());
+            System.out.println(String.format("Пользователь с id %d успешно обновлен.", id.get()));
         } catch (EntityNotFoundException ex){
             System.out.println(String.format("Пользователь с id %d не найден.", id.get()));
-        } catch (ConstraintViolationException ex){
+        } catch (org.hibernate.exception.ConstraintViolationException
+                 | jakarta.validation.ConstraintViolationException ex){
             System.out.println(String.format("При попытке изменить пользователя произошла ошибка: %s",
                     ex.getMessage()));
         }
