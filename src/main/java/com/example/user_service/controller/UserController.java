2be4238e22b805cbdd43d5d20d7127aa89e67d2c
DmitriyPershukov <dmitriypershukov@gmail.com>
2026-06-08 12:58:09 +0500

    moved exception handling logic from UserController to GlobalExceptionHandler

--- src/main/java/com/example/user_service/controller/UserController.java
+++ src/main/java/com/example/user_service/controller/UserController.java
@@ -62,34 +62,4 @@ public class UserController {
     ){
         userService.deleteUser(userId);
     }
-
-    @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity handleException(EntityNotFoundException ex) {
-        return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(ex.getMessage());
-    }
-
-    @ExceptionHandler(DataIntegrityViolationException.class)
-    public ResponseEntity handleException(DataIntegrityViolationException ex) {
-        String exceptionMessage = ex.getMostSpecificCause().getMessage();
-        String trimmedMessage = exceptionMessage.substring(exceptionMessage.indexOf("Detail")+8);
-        return ResponseEntity
-                .status(HttpStatus.CONFLICT)
-                .body(trimmedMessage);
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity handleException(MethodArgumentNotValidException ex) {
-        StringBuilder messageBuilder = new StringBuilder();
-        messageBuilder.append("Following field validation errors occurred:\n");
-        ex.getBindingResult().getFieldErrors().forEach(fieldError -> {
-            messageBuilder.append(String.format("Error in field '%s': %s\n",
-                    fieldError.getField(),
-                    fieldError.getDefaultMessage()));
-        });
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(messageBuilder);
-    }
 }
