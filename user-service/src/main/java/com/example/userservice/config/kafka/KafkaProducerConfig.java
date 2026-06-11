545bd7f64fc5032471eaad19ac34963ffed668d6
DmitriyPershukov <dmitriypershukov@gmail.com>
2026-06-11 17:32:02 +0500

    updated kafkaTemplate bean creation

--- user-service/src/main/java/com/example/userservice/config/kafka/KafkaProducerConfig.java
+++ user-service/src/main/java/com/example/userservice/config/kafka/KafkaProducerConfig.java
@@ -3,6 +3,7 @@ package com.example.userservice.config.kafka;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.kafka.autoconfigure.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -18,22 +19,13 @@ public class KafkaProducerConfig {
     private String bootstrapAddress;
 
     @Bean
-    public ProducerFactory<String, String> producerFactory() {
-        Map<String, Object> configProps = new HashMap<>();
-        configProps.put(
-                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                bootstrapAddress);
-        configProps.put(
-                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
-                StringSerializer.class);
-        configProps.put(
-                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
-                StringSerializer.class);
+    public ProducerFactory<String, String> producerFactory(KafkaProperties kafkaProperties) {
+        Map<String, Object> configProps = kafkaProperties.buildProducerProperties();
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
     @Bean
-    public KafkaTemplate<String, String> kafkaTemplate() {
-        return new KafkaTemplate<>(producerFactory());
+    public KafkaTemplate<String, String> kafkaTemplate(ProducerFactory<String, String> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
     }
 }
