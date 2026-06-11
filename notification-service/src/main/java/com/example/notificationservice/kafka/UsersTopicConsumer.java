aa815c24a53bac1bcf767263f9cceb7417b23598
DmitriyPershukov <dmitriypershukov@gmail.com>
2026-06-11 21:06:55 +0500

    moved notification email sending functionality to NotificationSender

--- notification-service/src/main/java/com/example/notificationservice/kafka/UsersTopicConsumer.java
+++ notification-service/src/main/java/com/example/notificationservice/kafka/UsersTopicConsumer.java
@@ -1,36 +1,19 @@
 package com.example.notificationservice.kafka;
 
+import com.example.notificationservice.mail.NotificationSender;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UsersTopicConsumer {
-    private static final String notificationSubject = "Изменение статуса аккаунта.";
-    private static final String createdNotification =
-            "Здравствуйте! Ваш аккаунт для user-service был успешно создан.";
-    private static final String deletedNotification= "Здравствуйте! Ваш аккаунт был удалён.";
+    private NotificationSender notificationSender;
 
-    private JavaMailSender emailSender;
-
-    public UsersTopicConsumer(JavaMailSender emailSender) {
-        this.emailSender = emailSender;
+    public UsersTopicConsumer(NotificationSender notificationSender) {
+        this.notificationSender = notificationSender;
     }
 
     @KafkaListener(topics = "users", groupId = "users.email-notification")
     public void listenUsers(String message) {
-        String event = message.split(" ")[0];
-        String to_address = message.split(" ")[1];
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setFrom("noreply@notificationservice.com");
-        mailMessage.setTo(to_address);
-        mailMessage.setSubject(notificationSubject);
-        if(event.equals("created")){
-            mailMessage.setText(createdNotification);
-        } else {
-            mailMessage.setText(deletedNotification);
-        }
-        emailSender.send(mailMessage);
+        notificationSender.sendNotification(message);
     }
 }
---
+++ notification-service/src/main/java/com/example/notificationservice/mail/NotificationSender.java
@@ -0,0 +1,34 @@
+package com.example.notificationservice.mail;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationSender {
+    private static final String notificationSubject = "Изменение статуса аккаунта.";
+    private static final String createdNotification =
+            "Здравствуйте! Ваш аккаунт для user-service был успешно создан.";
+    private static final String deletedNotification= "Здравствуйте! Ваш аккаунт был удалён.";
+
+    private JavaMailSender emailSender;
+
+    public NotificationSender(JavaMailSender emailSender) {
+        this.emailSender = emailSender;
+    }
+
+    public void sendNotification(String message){
+        String event = message.split(" ")[0];
+        String to_address = message.split(" ")[1];
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setFrom("noreply@notificationservice.com");
+        mailMessage.setTo(to_address);
+        mailMessage.setSubject(notificationSubject);
+        if(event.equals("created")){
+            mailMessage.setText(createdNotification);
+        } else {
+            mailMessage.setText(deletedNotification);
+        }
+        emailSender.send(mailMessage);
+    }
+}
