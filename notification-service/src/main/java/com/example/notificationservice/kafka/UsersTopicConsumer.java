0bba757ba1ce916e9829365482ab347d3049a1c6
DmitriyPershukov <dmitriypershukov@gmail.com>
2026-06-15 11:15:02 +0500

    topic and consumer group are now configured with env vars

--- notification-service/src/main/java/com/example/notificationservice/kafka/UsersTopicConsumer.java
+++ notification-service/src/main/java/com/example/notificationservice/kafka/UsersTopicConsumer.java
@@ -12,7 +12,7 @@ public class UsersTopicConsumer {
         this.messageProcessor = messageProcessor;
     }
 
-    @KafkaListener(topics = "users", groupId = "users.email-notification")
+    @KafkaListener(topics = "${kafka.topic}", groupId = "$kafka.group-id")
     public void listenUsers(String message) {
         messageProcessor.process(message);
     }
