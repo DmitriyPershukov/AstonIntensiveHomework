d7a83454df1058cd49537c773c1b0934dfbebd98
DmitriyPershukov <dmitriypershukov@gmail.com>
2026-06-22 16:16:30 +0500

    removed redundant dependencies

--- user-service/src/main/java/com/example/userservice/model/UserRepresentationModelAssembler.java
+++ user-service/src/main/java/com/example/userservice/model/UserRepresentationModelAssembler.java
@@ -1,18 +1,12 @@
 package com.example.userservice.model;
 
 import com.example.userservice.controller.UserController;
-import org.springframework.hateoas.Affordance;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.Link;
-import org.springframework.hateoas.LinkRelation;
-import org.springframework.hateoas.mediatype.Affordances;
 import org.springframework.hateoas.server.SimpleRepresentationModelAssembler;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
-import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.afford;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
